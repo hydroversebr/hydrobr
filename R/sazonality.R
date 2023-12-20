399ae1a811d399ea2e93c09d998ff3049c42e2f4
Arthur Calegario <tcalegario@gmail.com>
2023-12-20 19:17:50 -0300

    atualizacao_sazonality

--- R/sazonality.R
+++ R/sazonality.R
@@ -20,61 +20,110 @@
 
 sazonality = function(selectionsResultSeries, statistic = "median"){
 
-  MedianasMensais = function(x){
-
-    mediaMovel = x %>%
+  MedianasMensais <- function(x) {
+    mediaMovel <- x %>%
       dplyr::ungroup() %>%
       dplyr::mutate(mes = lubridate::month(monthCivilYear)) %>%
       dplyr::group_by_at(c("station_code", "mes")) %>%
-      dplyr::summarise(mediaMes = base::mean(stream_flow_m3_s, na.rm = TRUE),
-                medianaMes = stats::median(stream_flow_m3_s, na.rm = TRUE)) %>%
+      dplyr::summarise(mediaMes = base::mean(stream_flow_m3_s,
+                                             na.rm = TRUE
+      ), medianaMes = stats::median(stream_flow_m3_s,
+                                    na.rm = TRUE
+      )) %>%
       base::suppressMessages() %>%
-      dplyr::slice(rep(1:dplyr::n(), 2)) %>% #repetir data_frame
-      dplyr::mutate(mediaMovel6 = dplyr::if_else(statistic == "median",
-                                          zoo::rollapply(.data$medianaMes,
-                                                         6, FUN = base::mean,
-                                                         partial = T,
-                                                         align = "left"),
-                                          zoo::rollapply(.data$mediaMes,
-                                                         6, FUN = base::mean,
-                                                         partial = T,
-                                                         align = "left")),
-             meses = zoo::rollapply(.data$mes,
-                                    6, FUN = function(y) paste(dplyr::first(y), dplyr::last(y), sep = "-"),
-                                    partial = T,
-                                    align = "left")) %>%
+      dplyr::slice(rep(1:dplyr::n(), 2)) %>%
+      dplyr::mutate(
+        mediaMovel6 = zoo::rollapply(.data$medianaMes, 6,
+                                     FUN = base::mean,
+                                     partial = T, align = "left"
+        ),
+        medianaMovel6 = zoo::rollapply(.data$mediaMes,
+                                       6,
+                                       FUN = base::mean, partial = T, align = "left"
+        ),
+        meses = zoo::rollapply(.data$mes, 6, FUN = function(y) {
+          paste(dplyr::first(y),
+                dplyr::last(y),
+                sep = "-"
+          )
+        }, partial = T, align = "left")
+      ) %>%
       dplyr::slice(1:12)
+  }
+
+  if(statistic == "median"){
+
+    AnoHS <- lapply(selectionsResultSeries, FUN = MedianasMensais) %>%
+      dplyr::bind_rows() %>%
+      dplyr::group_by(station_code) %>%
+      dplyr::select(station_code, mes, medianaMes, medianaMovel6, meses) %>%
+      dplyr::filter(medianaMovel6 == min(medianaMovel6)) %>%
+      dplyr::select(1,2,5) %>%
+      stats::setNames(c(
+        "station_code", "FirstDryMonth",
+        "DryMonths"
+      ))
+
+    AnoHU <- lapply(selectionsResultSeries, FUN = MedianasMensais) %>%
+      dplyr::bind_rows() %>%
+      dplyr::group_by(station_code) %>%
+      dplyr::select(station_code, mes, medianaMes, medianaMovel6, meses) %>%
+      dplyr::filter(medianaMovel6 == max(medianaMovel6)) %>%
+      dplyr::select(
+        1,
+        2, 5
+      ) %>%
+      stats::setNames(c(
+        "station_code", "FirstwetMonth",
+        "WetMonths"
+      ))
+
+  } else {
 
 
+    AnoHS <- lapply(selectionsResultSeries, FUN = MedianasMensais) %>%
+      dplyr::bind_rows() %>%
+      dplyr::group_by(station_code) %>%
+      dplyr::select(station_code, mes, mediaMes, mediaMovel6, meses) %>%
+      dplyr::filter(mediaMovel6 == min(mediaMovel6)) %>%
+      dplyr::select(1,2,5) %>%
+      stats::setNames(c(
+        "station_code", "FirstDryMonth",
+        "DryMonths"
+      ))
+
+    AnoHU <- lapply(selectionsResultSeries, FUN = MedianasMensais) %>%
+      dplyr::bind_rows() %>%
+      dplyr::group_by(station_code) %>%
+      dplyr::select(station_code, mes, mediaMes, mediaMovel6, meses) %>%
+      dplyr::filter(mediaMovel6 == max(mediaMovel6)) %>%
+      dplyr::select(
+        1,
+        2, 5
+      ) %>%
+      stats::setNames(c(
+        "station_code", "FirstwetMonth",
+        "WetMonths"
+      ))
 
 
   }
 
-
-  AnoHS = lapply(selectionsResultSeries,FUN = MedianasMensais) %>%
-    dplyr::bind_rows() %>%
-    dplyr::group_by(station_code) %>%
-    dplyr::filter(mediaMovel6==min(mediaMovel6)) %>%
-    dplyr::select(1,2, 6) %>%
-    stats::setNames(c("station_code", "FirstDryMonth", "DryMonths"))
-
-
-  AnoHU = lapply(selectionsResultSeries,FUN = MedianasMensais) %>%
-    dplyr::bind_rows() %>%
-    dplyr::group_by(station_code) %>%
-    dplyr::filter(mediaMovel6==max(mediaMovel6)) %>%
-    dplyr::select(1,2, 6) %>%
-    stats::setNames(c("station_code", "FirstwetMonth", "WetMonths"))
-
-  Sazonalidade = dplyr::left_join(AnoHU, AnoHS, by = "station_code") %>%
-    dplyr::select(c(1,2,4,3,5))
+  Sazonalidade <- dplyr::left_join(AnoHU, AnoHS, by = "station_code") %>%
+    dplyr::select(c(1, 2, 4, 3, 5)) %>%
+    dplyr::ungroup()
 
   return(Sazonalidade)
-
 }
 
 
+
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("monthCivilYear",
+                                                        "mes",
+                                                        "medianaMes",
+                                                        "medianaMovel6",
+                                                        "meses",
+                                                        "mediaMes",
                                                         'selectionsResult',
                                                         'mediaMovel6',
                                                         'lastDateQ7',
