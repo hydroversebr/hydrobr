e3e334df063b2ea40cfbde878c6254b4aa8f4d09
Arthur Calegario <tcalegario@gmail.com>
2024-03-07 17:36:29 -0300

    refresh historicalStatisticsZonal

--- R/historicalStatisticsSazonal.R
+++ R/historicalStatisticsSazonal.R
@@ -144,7 +144,7 @@ historicalStatisticsSazonal = function(selectStationsResultSeries,
       dplyr::mutate("{statistics}_m3_s_first6_months" := b[2] %>% dplyr::pull(),
                     "{statistics}_m3_s_last6_months" := c[2] %>% dplyr::pull(),
                     "{statistics}_first6m_ratio_{statistics}_hist_porcent" := round(b[2]/a[2]*100,1) %>% dplyr::pull(),
-                    "{statistics}_first6m_ratio_{statistics}_hist_porcent" := round(c[2]/a[2]*100,1) %>% dplyr::pull(),
+                    "{statistics}_last6m_ratio_{statistics}_hist_porcent" := round(c[2]/a[2]*100,1) %>% dplyr::pull(),
                     InicialMonth = startMonth)
 
 
