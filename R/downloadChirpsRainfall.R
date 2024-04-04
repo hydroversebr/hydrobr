9add9610cc62c31b52448813bd75cb5c9ebbb505
Arthur Telles Calegario <tcalegario@gmail.com>
2024-04-04 12:01:13 -0300

    correcao_besta

--- R/downloadChirpsRainfall.R
+++ R/downloadChirpsRainfall.R
@@ -39,39 +39,39 @@
 #'@export
 #'
 downloadChirpsRainfall = function(dir_out, years, aoi){
-    
-    
+
+
     stopifnot(
       "`dir_out` parameter must be character indicating output folder path (i.e `c:/temp`)" = is.character(dir_out),
       "`years` must be numeric vector containing years to be downloaded" = is.numeric(years),
       "`aoi` must be a polygon of class `SpatVector` (terra package)" = class(aoi) == "SpatVector")
-    
-    
-    
+
+
+
     dir.create(dir_out, showWarnings = FALSE)
-    
-        
+
+
     for (i in 1:length(years)){
-      
+
       print(paste0(years[i], " Downloading"))
-      
+
       inicial = paste0(years[i], "-01","-01")
-      
+
       final = paste0(years[i], "-12","-31")
-      
+
       if(years[i] == 1981){
-        
+
         inicial = paste0(years[i], "-01", "-02")
-        
+
       }
-      
-      
+
+
       dates = c(inicial, final)
-      
+
       chirpsData <- chirps::get_chirps(aoi, dates, server = "CHC", as.raster = TRUE)
-      
+
       print(paste0(years[i], " Saving"))
-      
+
       terra::writeRaster(chirpsData, filename = paste0(dir_out,
                                                        "/chirps_rainfall", years[i], ".tif"),
                          filetype = "GTiff", overwrite = TRUE)
@@ -106,4 +106,4 @@ downloadChirpsRainfall = function(dir_out, years, aoi){
 
 
 
-}
+
